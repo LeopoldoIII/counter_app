325fc2df1608f4a5753f63d3cdee08f337393330
LeopoldoIII <nathan.rx@gmail.com>
2022-04-15 17:50:48 -0500

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class HomeSreen extends StatelessWidget {
+  const HomeSreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('HomeScreen'),
+    );
+  }
+}
