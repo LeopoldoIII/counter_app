aac4a065ecbc25054f60450ccf2927cbb6d0f005
LeopoldoIII <nathan.rx@gmail.com>
2022-04-15 18:18:55 -0500

    05 buildContext scaffold

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 class HomeSreen extends StatelessWidget {
+  
   const HomeSreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('HomeScreen'),
+    return const Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Center(
+        child: Text('HomeScreen'),
+      ),
     );
   }
 }
