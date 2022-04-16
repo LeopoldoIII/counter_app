e7454ef26c9005c17a5dee21e606faebd4c2d1bd
LeopoldoIII <nathan.rx@gmail.com>
2022-04-15 20:41:34 -0500

    Columns styles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,11 +6,24 @@ class HomeSreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.indigo,
-      body: Center(
-        child: Text('HomeScreen'),
+
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        backgroundColor: Colors.indigo,
+        elevation: 10.0,
+      ),
+      body:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: const <Widget>[
+            Text('Text 1'),
+            Text('Text 2'),
+          ],
+        ),
       ),
     );
   }
+
 }
