6a4139779fffce888755cecab76cccbce6c3904e
LeopoldoIII <nathan.rx@gmail.com>
2022-04-18 14:28:03 -0500

    floating action button added

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -29,14 +29,34 @@ class _CounterScreenState extends State<CounterScreen> {
           ],
         ),
       ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          counter++;
-          setState(() {
-          });
-        },
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            child: const Icon(Icons.exposure_plus_1_outlined),
+            onPressed: () {
+              counter++;
+              setState(() {});
+            },
+          ),
+
+          FloatingActionButton(
+            child: const Icon(Icons.exposure_zero),
+            onPressed: () {
+              counter = 0;
+              setState(() {});
+            },
+          ),
+
+          FloatingActionButton(
+            child: const Icon(Icons.exposure_minus_1_outlined),
+            onPressed: () {
+              counter--;
+              setState(() {});
+            },
+          ),
+        ],
       ),
     );
   }
